be2f266b2b958b0558b6577d9f58bdc6600af659
fronald143 <120772118+fronald143@users.noreply.github.com>
2024-03-06 20:38:46 +0800

    new game code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {useEffect, useState } from 'react';
 import BingoCard from './BingoCard';
 
 function App() {
-  const url = 'http://www.hyeumine.com/getcard.php?bcode=8NRcsvwI';
+  const url = 'http://www.hyeumine.com/getcard.php?bcode=iXenW1hm';
   const winCheckUrl = 'http://www.hyeumine.com/checkwin.php';
   const let_colors = ['#B7C9F2', '#93ccb6', '#FFDD95', '#E6BAA3', '#d197b4'];
   const card_colors = ['#9195F6', '#5C8374', '#FF9843', '#D24545', '#85586F' ];
@@ -63,7 +63,7 @@ function App() {
   return (
     <div className="Con">
       <h3 style={{margin:"0"}}>Player's Cards</h3>
-      <h4 style={{margin:"0"}}>Game Code: 8NRcsvwI</h4>
+      <h4 style={{margin:"0"}}>Game Code: iXenW1hm</h4>
       <h5 style={{margin:"0",marginBottom:"20px", color:"green"}}>{winner}</h5>
       <div className="App">
         {cards.map((card, index) => (
@@ -84,7 +84,7 @@ function App() {
         <button className="btnNewCard" onClick={() => handleNewCard()}>New Card</button>
         <button className="btnClearCard" onClick={() => handleClearCard()}>Clear Cards</button>
         <button className="btnNewPlayer" onClick={() => window.open('http://localhost:3000', '_blank')}>New Player</button>
-        <button className="btnNavigate" onClick={() => window.location.href = 'http://www.hyeumine.com/bingodashboard.php?bcode=8NRcsvwI'}>Go to Bingo Dashboard</button>
+        <button className="btnNavigate" onClick={() => window.location.href = 'http://www.hyeumine.com/bingodashboard.php?bcode=iXenW1hm'}>Go to Bingo Dashboard</button>
       </div>
     </div>
   );
